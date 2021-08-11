a50de5f945d78bdecd7aede0bd21a34e18932423
João Costa <joaocosta@zonalivre.org>
2021-08-11 08:30:44 +0100

    Support running from any location

--- boot.sh
+++ boot.sh
@@ -2,21 +2,23 @@
 
 set -euo pipefail
 
-ANSIBLE_VAULT_PASS=`cat vpass`
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-sed "s/ANSIBLE_VAULT_PASS/$ANSIBLE_VAULT_PASS/g" fx-centos8.ks > ks.cfg
+ANSIBLE_VAULT_PASS=`cat $SCRIPT_DIR/vpass`
+
+sed "s/ANSIBLE_VAULT_PASS/$ANSIBLE_VAULT_PASS/g" $SCRIPT_DIR/fx-centos8.ks > $SCRIPT_DIR/ks.cfg
 
 virt-install \
     --name fxdev00 \
     --memory 8192 \
     --vcpus 2 \
     --disk size=40 \
-    --location /home/joao/src/boot/CentOS-8.4.2105-x86_64-dvd1.iso \
+    --location $SCRIPT_DIR/CentOS-8.4.2105-x86_64-dvd1.iso \
     --os-variant centos8 \
     --graphics none \
-    --initrd-inject /home/joao/src/boot/ks.cfg \
+    --initrd-inject $SCRIPT_DIR/ks.cfg \
     --console pty,target_type=serial \
     --extra-args 'console=ttyS0,115200n8 serial' \
     --extra-args 'inst.ks=file:/ks.cfg console=tty0 console=ttyS0,115200n8'
 
-rm ks.cfg
+rm $SCRIPT_DIR/ks.cfg
